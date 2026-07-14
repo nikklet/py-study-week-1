25fecfd0069e7e60feceb8a4325399899db70888
nikklet <onwukwetc@email.com>
2026-07-14 15:23:53 +0100

    last edit -rounding up floats

--- calculator.py
+++ calculator.py
@@ -7,3 +7,45 @@ print(z)
 
 z = int(x) + int(y)
 print(z)
+
+# a shorter version of the above code
+x = int(input("what's x? "))
+y = int(input("what's y? "))
+
+print(x + y)
+
+# using decimal point with float
+x = float(input("what's x? "))
+y = float(input("what's y? "))
+
+print(x + y)
+
+# rounding up integers 
+x = float(input("what's x? "))
+y = float(input("what's y? "))
+
+z = round(x + y)
+
+print(z)
+
+# adding comma with the f= format 
+x = float(input("what's x? "))
+y = float(input("what's y? "))
+
+z = round(x + y)
+# use print(z) if u dont want comma
+print(f"{z:,}")
+
+#
+x = float(input("what's x? "))
+y = float(input("what's y? "))
+
+z = x / y
+print(z)
+
+# to decide the nuber of digits you want to round up to
+z = round(x /y, 2)
+print(z)
+# or
+z = x / y
+print(f"{z:.3f}")
