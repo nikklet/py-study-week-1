12ba1f482ba6ea3bc11385fc013c8a1d62b73ab8
nikklet <onwukwetc@email.com>
2026-07-17 19:24:08 +0100

    match statements

---
+++ house.py
@@ -0,0 +1,23 @@
+name = input("what is your name? ")
+
+match name:
+    case "Harry":
+        print("Gryffindor")
+    case "Hermione":
+        print("Gryffindor")
+    case "Ron":
+        print("Gryffindor")
+    case "Draco":
+        print("Slytherin")
+    case _:
+        print("Who?")
+
+# shorter version
+# match can be used instead of if else statements
+match name:
+    case "Harry" | "Hermione" | "Ron":
+        print("Gryffindor")
+    case "Draco":
+        print("Slytherin")
+    case _:
+        print("Who?")
